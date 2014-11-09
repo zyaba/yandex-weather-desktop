78c8c64a9482bfadae8035f2bda9bc9e7b8d7eea
Andrey Boreyko <boreykoandrey@gmail.com>
2014-11-09 13:17:31 +0500

    Added 'jshint' and 'jscs' to grunt (Fixed #1)

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,33 +1,55 @@
-module.exports = function(grunt) {
+module.exports = function (grunt) {
 
     grunt.initConfig({
-      cssmin: {
-          my_target: {
-            files: [{
-              expand: true,
-              cwd: 'dist/',
-              src: ['weather.css', '!*.min.css'],
-              dest: 'dist/',
-              ext: '.min.css'
-            }]
-          }
+        cssmin: {
+            my_target: {
+                files: [
+                    {
+                        expand: true,
+                        cwd: 'dist/',
+                        src: ['weather.css', '!*.min.css'],
+                        dest: 'dist/',
+                        ext: '.min.css'
+                    }
+                ]
+            }
         },
 
-    stylus: {
-      compile: {
-        options: {
-          // paths: ['path/to/import', 'another/to/import'],
+        stylus: {
+            compile: {
+                options: {
+                    // paths: ['path/to/import', 'another/to/import'],
+                },
+                files: {
+                    'dist/weather.css': 'static/**/*.styl' // 1:1 compile
+                }
+            }
         },
-        files: {
-          'dist/weather.css': 'static/**/*.styl' // 1:1 compile
+
+        jscs: {
+            src: "static/**/*.js",
+            options: {
+                config: true,
+                requireCurlyBraces: [ "if" ]
+            }
+        },
+
+        jshint: {
+            options: {
+                jshintrc: '.jshintrc' // relative to Gruntfile
+            },
+            // You get to make the name
+            // The paths tell JSHint which files to validate
+            myFiles: ['static/**/*.js']
         }
-      }
-    }
     });
 
     grunt.loadNpmTasks('grunt-contrib-cssmin');
     grunt.loadNpmTasks('grunt-contrib-stylus');
+    grunt.loadNpmTasks('grunt-contrib-jshint');
+    grunt.loadNpmTasks("grunt-jscs");
 
     grunt.registerTask('default', ['stylus', 'cssmin']);
+    grunt.registerTask('process_js', ['jscs', 'jshint'] );
 
 };
