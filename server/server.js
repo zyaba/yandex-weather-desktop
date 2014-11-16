012406e944a74f562365d41cb9b5f54547404b15
drzom <zoom.site.design@gmail.com>
2014-11-16 15:48:51 +0500

    test commit

--- server/server.js
+++ server/server.js
@@ -1,15 +1,15 @@
-var express = require('express'),
-    http = require('http');
+var express = require("express"),
+    http = require("http");
 
 var app = express(); 
 var server = http.createServer(app);
 
-app.use("/", express.static(__dirname + '/../'));
-app.use("/dist", express.static(__dirname + '/../dist'));
+app.use("/", express.static(__dirname + "/../"));
+app.use("/dist", express.static(__dirname + "/../dist"));
 
-app.get('/', function(req, res){
+app.get("/", function(req, res){
     //res.send('hello world');
-    res.sendFile('index.html');
+    res.sendFile("index.html");
 });
 
 app.listen(8080);
