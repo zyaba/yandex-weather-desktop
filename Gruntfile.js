3babf930032a8f76cd11bfd5e4076b1d0c5bf8e5
drzom <zoom.site.design@gmail.com>
2014-11-16 17:00:00 +0500

    fix commas

--- Gruntfile.js
+++ Gruntfile.js
@@ -4,7 +4,7 @@ module.exports = function (grunt) {
         stylus: {
           compile: {
             files: {
-              'dist/temp/temp.css': 'static/**/*.styl'
+              "dist/temp/temp.css": "static/**/*.styl"
             }
           }
         },
@@ -12,11 +12,11 @@ module.exports = function (grunt) {
         autoprefixer: {
             single_file: {
                 options: {
-                  browsers: ['> 1%', 'last 2 versions', 'Firefox ESR', 'Opera 12.1', 'ie 8', 'ie 9']
+                  browsers: ["> 1%", "last 2 versions", "Firefox ESR", "Opera 12.1", "ie 8", "ie 9"]
                 },
 
-                src: 'dist/temp/temp.css',
-                dest: 'dist/temp/weather.css'
+                src: "dist/temp/temp.css",
+                dest: "dist/temp/weather.css"
             }
         },
 
@@ -24,10 +24,10 @@ module.exports = function (grunt) {
           my_target: {
             files: [{
               expand: true,
-              cwd: 'dist/temp/',
-              src: ['weather.css', '!*.min.css'],
-              dest: 'dist/',
-              ext: '.min.css'
+              cwd: "dist/temp/",
+              src: ["weather.css", "!*.min.css"],
+              dest: "dist/",
+              ext: ".min.css"
             }]
           }
         },
@@ -37,8 +37,8 @@ module.exports = function (grunt) {
               reload: true
             },
             src: {
-              files: ['static/**/*.styl'],
-              tasks: ['default'],
+              files: ["static/**/*.styl"],
+              tasks: ["default"],
             }
         },
 
@@ -52,14 +52,14 @@ module.exports = function (grunt) {
 
         jshint: {
             options: {
-                jshintrc: '.jshintrc' // relative to Gruntfile
+                jshintrc: ".jshintrc" // relative to Gruntfile
             },
             // You get to make the name
             // The paths tell JSHint which files to validate
-            myFiles: ['static/**/*.js']
+            myFiles: ["static/**/*.js"]
         },
 
-        clean: ['dist/temp/'],
+        clean: ["dist/temp/"],
         
         shell: {
             server: {
@@ -67,28 +67,28 @@ module.exports = function (grunt) {
                     stdout: true
                 },
                 command: [
-                    'cd server',
-                    'node server'
-                ].join('&&')
+                    "cd server",
+                    "node server"
+                ].join("&&")
             }
         }
     });
 
-    grunt.loadNpmTasks('grunt-contrib-cssmin');
-    grunt.loadNpmTasks('grunt-contrib-stylus');
-    grunt.loadNpmTasks('grunt-autoprefixer');
-    grunt.loadNpmTasks('grunt-contrib-watch');
-    grunt.loadNpmTasks('grunt-contrib-clean');
-    grunt.loadNpmTasks('grunt-shell');
+    grunt.loadNpmTasks("grunt-contrib-cssmin");
+    grunt.loadNpmTasks("grunt-contrib-stylus");
+    grunt.loadNpmTasks("grunt-autoprefixer");
+    grunt.loadNpmTasks("grunt-contrib-watch");
+    grunt.loadNpmTasks("grunt-contrib-clean");
+    grunt.loadNpmTasks("grunt-shell");
 
-    grunt.registerTask('default', ['stylus', 'autoprefixer', 'cssmin', 'clean']);
+    grunt.registerTask("default", ["stylus", "autoprefixer", "cssmin", "clean"]);
 
-    grunt.loadNpmTasks('grunt-contrib-jshint');
+    grunt.loadNpmTasks("grunt-contrib-jshint");
     grunt.loadNpmTasks("grunt-jscs");
 
-    grunt.registerTask('process_js', ['jscs', 'jshint'] );
+    grunt.registerTask("process_js", ["jscs", "jshint"] );
     
     /* Start Express server */
-    grunt.registerTask('express', ['shell']);
+    grunt.registerTask("express", ["shell"]);
         
 };
