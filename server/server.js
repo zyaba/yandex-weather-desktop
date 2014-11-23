9d96ce3926b52d0c072c2f0aa9b5983ed3251e63
Andrey Boreyko <boreykoandrey@gmail.com>
2014-11-23 17:46:47 +0500

    removed 'static' folder

--- server/server.js
+++ server/server.js
@@ -7,7 +7,6 @@ app.set('view engine', 'jsx');
 app.engine('jsx', require('express-react-views').createEngine());
 
 app.use('/dist', express.static(__dirname + '/../dist'));
-app.use('/static', express.static(__dirname + '/../static'));
 app.use('/bower_components', express.static(__dirname + '/../bower_components'));
 
 app.get('/', require('./routes').index);
