46e742edbc7a6e940027417e9e9c0014d0037bc5
Andrey Boreyko <boreykoandrey@gmail.com>
2014-11-23 17:33:06 +0500

    added some test data; added 'full forecast' partial;

--- server/server.js
+++ server/server.js
@@ -7,8 +7,10 @@ app.set('view engine', 'jsx');
 app.engine('jsx', require('express-react-views').createEngine());
 
 app.use('/dist', express.static(__dirname + '/../dist'));
+app.use('/static', express.static(__dirname + '/../static'));
 app.use('/bower_components', express.static(__dirname + '/../bower_components'));
 
 app.get('/', require('./routes').index);
+app.get('/:geoid', require('./routes').geoid );
 
 app.listen(8080);
