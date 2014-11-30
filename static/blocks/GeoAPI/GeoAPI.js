3d1cebba3e8ac4f430ba772344e5ec1c26df4715
user <user@213.180.221.72-ekb.dhcp.yndx.net>
2014-11-30 16:26:21 +0500

    save cities in local storage

--- static/blocks/GeoAPI/GeoAPI.js
+++ static/blocks/GeoAPI/GeoAPI.js
@@ -3,24 +3,51 @@ var GeoAPI = (function () {
     return {
         getGeoId: function() {
             var promise = $.Deferred();
+            var weather = null;
+            if ('localStorage' in window && window['localStorage'] !== null) {
+                weather = JSON.parse(window.localStorage.getItem ("yaWeather")) || {};
+            }
             
-            if (navigator.geolocation) {
-                navigator.geolocation.getCurrentPosition(function(position) {                
-                    if (position && position.coords) {
-                        $.get( "/locality", { latitude: position.coords.latitude, longitude: position.coords.longitude}).done(function( data ) {
-                            promise.resolve(data.geoid);
-                        }).fail(function(error) {promise.resolve(MOSCOW_ID);});
-                    } else {
+            if (weather && weather.cities) {
+                var city = weather.cities.filter(function(object, i) {
+                    return object.last == true;
+                })[0];
+                promise.resolve(city.id);
+                
+            } else {            
+                if (navigator.geolocation) {
+                    navigator.geolocation.getCurrentPosition(function(position) {                
+                        if (position && position.coords) {
+                            $.get( "/locality", { latitude: position.coords.latitude, longitude: position.coords.longitude}).done(function( data ) {
+                                if (weather) {
+                                    if (!weather.cities) weather.cities = [];
+                                    var inArray = false;
+                                    weather.cities.map(function(object, i) {
+                                        object.last = false;
+                                        if (object.id == data.geoid) {object.last = true; inArray = true;}
+                                    })
+                                    if (!inArray) {
+                                        weather.cities.push({id: data.geoid, last: true})
+                                    }
+                                    
+                                    window.localStorage.setItem("yaWeather",JSON.stringify(weather));
+                                }
+                                
+
+                                promise.resolve(data.geoid);
+                            }).fail(function(error) {promise.resolve(MOSCOW_ID);});
+                        } else {
+                            promise.resolve(MOSCOW_ID);
+                        }
+
+                    }, function(error) {
                         promise.resolve(MOSCOW_ID);
-                    }
+                    });
 
-                }, function(error) {
+                } else {
                     promise.resolve(MOSCOW_ID);
-                });
-
-            } else {
-                promise.resolve(MOSCOW_ID);
-            };
+                };
+            }
             
             return promise.promise();
         }
