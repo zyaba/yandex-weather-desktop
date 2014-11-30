7ed5a58b0fe3ab2a71ddd92073de9ef0f0541d56
user <user@213.180.221.72-ekb.dhcp.yndx.net>
2014-11-30 15:21:47 +0500

    Fix GeoAPI errors

--- static/blocks/GeoAPI/GeoAPI.js
+++ static/blocks/GeoAPI/GeoAPI.js
@@ -1,5 +1,5 @@
 var GeoAPI = (function () {
-    
+    var MOSCOW_ID = 213;
     return {
         getGeoId: function() {
             var promise = $.Deferred();
@@ -9,17 +9,17 @@ var GeoAPI = (function () {
                     if (position && position.coords) {
                         $.get( "/locality", { latitude: position.coords.latitude, longitude: position.coords.longitude}).done(function( data ) {
                             promise.resolve(data.geoid);
-                        }).fail(function(error) {promise.resolve(213);});
+                        }).fail(function(error) {promise.resolve(MOSCOW_ID);});
                     } else {
-                        promise.resolve(213);
+                        promise.resolve(MOSCOW_ID);
                     }
 
                 }, function(error) {
-                    promise.resolve(213);
+                    promise.resolve(MOSCOW_ID);
                 });
 
             } else {
-                promise.resolve(213);
+                promise.resolve(MOSCOW_ID);
             };
             
             return promise.promise();
